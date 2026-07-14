0965db4f97d5a20b1ff855cc575791ffda7ac68f
michelelorandi <michele.lor.74@gmail.com>
2026-07-14 10:48:37 +0200

    Update script.js

--- I_disegni_a_china_di_Michele_Lorandi/js/script.js
+++ I_disegni_a_china_di_Michele_Lorandi/js/script.js
@@ -46,3 +46,43 @@ window.addEventListener("scroll", () => {
     }
 
 });// pronto per espansione
+/* ===========================
+   ANIMAZIONI
+=========================== */
+
+.opera{
+
+    opacity:0;
+    transform:translateY(40px);
+
+}
+
+.opera.show{
+
+    opacity:1;
+    transform:translateY(0);
+
+}
+
+.opera{
+
+    transition:
+        opacity .8s ease,
+        transform .8s ease,
+        box-shadow .35s;
+
+}
+
+/* Header */
+
+header{
+
+    transition:.35s;
+
+}
+
+header.scroll{
+
+    box-shadow:0 8px 24px rgba(0,0,0,.10);
+
+}
