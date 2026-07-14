c489d428564a46403c2b263eda531af42c208c74
michelelorandi <michele.lor.74@gmail.com>
2026-07-14 10:46:24 +0200

    Update script.js

--- I_disegni_a_china_di_Michele_Lorandi/js/script.js
+++ I_disegni_a_china_di_Michele_Lorandi/js/script.js
@@ -1,1 +1,48 @@
-// pronto per espansione
+/* ===========================================
+   I DISEGNI A CHINA
+   Michele Lorandi
+=========================================== */
+
+// Animazione delle opere
+
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach(entry => {
+
+        if (entry.isIntersecting) {
+
+            entry.target.classList.add("show");
+
+        }
+
+    });
+
+}, {
+
+    threshold: 0.15
+
+});
+
+document.querySelectorAll(".opera").forEach((opera) => {
+
+    observer.observe(opera);
+
+});
+
+// Header durante lo scroll
+
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 40) {
+
+        header.classList.add("scroll");
+
+    } else {
+
+        header.classList.remove("scroll");
+
+    }
+
+});// pronto per espansione
